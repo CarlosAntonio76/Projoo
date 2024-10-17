bd06417506b524753cfd1c574cb8c27e4904bed6
Carlos Antonio <carlosantoniodapaixao@gmail.com>
2024-10-17 18:43:21 -0300

    Funçao calcular - treinando

---
+++ oo/calcular.py
@@ -0,0 +1,22 @@
+class Calcular:
+    def soma(x, y):
+        return x + y
+    
+    def subtrai(x, y):
+        return x - y
+    
+    def multiplica(x, y):
+        return x * y
+    
+    def dividir(x, y):
+        return x /  y
+
+
+
+calculo = Calcular
+
+
+print(calculo.soma(10, 8))
+print(calculo.subtrai(10, 8))
+print(calculo.multiplica(5, 5))
+print(calculo.dividir(8, 2))    
